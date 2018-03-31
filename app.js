66d5ed668a50077cb0671a002b7daba252d5d7dc
unknown <bubbastinx@gmail.com>
2018-03-31 13:06:37 -0600

    click event works, and everything works

--- app.js
+++ app.js
@@ -8,37 +8,37 @@ $(document).ready(function(){
     //googole api key and access link
     // <script src="https://maps.googleapis.com/maps/api/js?sensor=true&key=AIzaSyBSnJtTqZp2Nzg7w1o1rF19y2Eic3IuhCQ&libraries=places"></script> 
 
-    // Start of Geolocation fucntion
-    navigator.geolocation.getCurrentPosition(function(position) {
+  // Start of Geolocation fucntion
+  navigator.geolocation.getCurrentPosition(function(position) {
 
-      let currentLat = position.coords.latitude
-      let currentLng = position.coords.longitude
+    let currentLat = position.coords.latitude
+    let currentLng = position.coords.longitude
 
-      // console.log(currentLat, currentLng)
-      let fetchUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=breweries&latitude=${currentLat}&longitude=${currentLng}`
+    // console.log(currentLat, currentLng)
 
-      let myHeaders = new Headers();
-      myHeaders.append("Authorization", "Bearer " + apiKey);
+    
+    const apiKey = "IB4MtYCaYXdQIdqm4K7847xEzhASkSEll2GFdl2tKVcElY8dSP3w-LCa03qSscEkwKVncUnsR5AizTA7EdD7FHmM1Qsr781Rsc3EqeKCIDw7jd8PFMRNaK1OwXS6WnYx"
+    let fetchUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=breweries&latitude=${currentLat}&longitude=${currentLng}`
 
-      fetch(fetchUrl, {
-        headers: myHeaders 
-      }).then((res) => {
-        re
-      
-      const apiKey = "IB4MtYCaYXdQIdqm4K7847xEzhASkSEll2GFdl2tKVcElY8dSP3w-LCa03qSscEkwKVncUnsR5AizTA7EdD7FHmM1Qsr781Rsc3EqeKCIDw7jd8PFMRNaK1OwXS6WnYx"
-turn res.json();
-      }).then((json) => {
-        console.log(json);
-      });
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + apiKey);
 
+    fetch(fetchUrl, {
+      headers: myHeaders 
+    }).then((res) => {
+      return res.json();
+    }).then((json) => {
+      console.log(json);
     });
 
+  });
 
   
 $("#searchButton").on("click", function(e) {
  console.log("You clicked the Search Button!");
  let userSearchTitle = $("#searchField").val();
-console.log(userSearchTitle);
+  // navigator.geolocation.getCurrentPosition();
+  // console.log(navigator.geolocation.getCurrentPosition());
 
  event.preventDefault();
 });
