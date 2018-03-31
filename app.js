ecbf67ee28d6d2ce576b6293e63b11ba920c63b1
unknown <bubbastinx@gmail.com>
2018-03-31 12:20:40 -0600

    created a search box, added ids to search box, and search fields, created a click function that capture search box value

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+    //googole api key and access link
+    // <script src="https://maps.googleapis.com/maps/api/js?sensor=true&key=AIzaSyBSnJtTqZp2Nzg7w1o1rF19y2Eic3IuhCQ&libraries=places"></script> 
+
     // Start of Geolocation fucntion
     navigator.geolocation.getCurrentPosition(function(position) {
 
@@ -11,9 +15,6 @@ $(document).ready(function(){
       let currentLng = position.coords.longitude
 
       // console.log(currentLat, currentLng)
-
-      
-      const apiKey = "IB4MtYCaYXdQIdqm4K7847xEzhASkSEll2GFdl2tKVcElY8dSP3w-LCa03qSscEkwKVncUnsR5AizTA7EdD7FHmM1Qsr781Rsc3EqeKCIDw7jd8PFMRNaK1OwXS6WnYx"
       let fetchUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=breweries&latitude=${currentLat}&longitude=${currentLng}`
 
       let myHeaders = new Headers();
@@ -22,9 +23,22 @@ $(document).ready(function(){
       fetch(fetchUrl, {
         headers: myHeaders 
       }).then((res) => {
-        return res.json();
+        re
+      
+      const apiKey = "IB4MtYCaYXdQIdqm4K7847xEzhASkSEll2GFdl2tKVcElY8dSP3w-LCa03qSscEkwKVncUnsR5AizTA7EdD7FHmM1Qsr781Rsc3EqeKCIDw7jd8PFMRNaK1OwXS6WnYx"
+turn res.json();
       }).then((json) => {
         console.log(json);
       });
 
     });
+
+
+  
+$("#searchButton").on("click", function(e) {
+ console.log("You clicked the Search Button!");
+ let userSearchTitle = $("#searchField").val();
+console.log(userSearchTitle);
+
+ event.preventDefault();
+});
