7047a4c90b434cdeff10a47122b4638731e0b596
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-12 19:31:25 -0500

    formatted code

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
-  origin: process.env.ORIGIN_URL || "http://localhost",
-  optionsSuccessStatus: 200
+    origin: process.env.ORIGIN_URL || "http://localhost",
+    optionsSuccessStatus: 200
 };
 
 app.use(express.static("public"));
 
 app.use("/cors/*", function(req, res) {
-  req.pipe(request(req.params[0])).pipe(res);
+    req.pipe(request(req.params[0])).pipe(res);
 });
 
 app.listen(PORT, function() {
-  console.log(`CORS-enabled web server listening on port ${PORT}`);
+    console.log(`CORS-enabled web server listening on port ${PORT}`);
 });
